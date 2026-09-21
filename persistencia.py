befac7b6a0ba2ceb657e0fb7f471217052c400a8
Mari-Reyes <marianittauwu9515@gmail.com>
2026-09-21 14:47:14 -0500

    feat: :sparkles: Se escribío el código de la función 'Cargar Json'.
    Esta función permite abrir el archivo Json, nos permite leerlo y cerrarlo automaticamente en caso de buscar un archivo inexistente, para que no falle y siga con su trabajo normal.

--- persistencia.py
+++ persistencia.py
@@ -3,14 +3,16 @@
 import json
  
 def guardar_json(ruta, datos):
-    """Guarda la lista de datos en el archivo JSON indicado."""
     with open (ruta, 'w', encoding='utf-8') as archivo:
         json.dump(datos, archivo, ensure_ascii=False, indent=2)
  
  
 def cargar_json(ruta):
-    """Carga y devuelve los datos del archivo JSON.
- 
-    Si el archivo no existe, devuelve una lista vacía [].
-    """
-    pass
+
+    try:
+        with open (ruta, 'r', encoding='utf-8') as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
